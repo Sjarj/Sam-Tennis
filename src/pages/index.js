4fe9252c6a9adabc75c2df0d701ac76ec8519ff4
Sebastien Jarjanette <sjarj@MacBook-Pro-de-Sebastien.local>
2019-07-18 16:26:23 +0200

    Add collapse

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,48 @@
 import React from "react"
-
+import { MDBContainer, MDBRow, MDBCol } from "mdbreact"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import CarouselPage from "../components/carousel"
+import Inscription from "../components/inscription"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <CarouselPage />
+    <MDBContainer>
+      <h1 className="m-5 text-center">
+        {" "}
+        Bonjour et Bienvenue sur le Site du{" "}
+        <span className="blue-text">SAM </span>
+        <span className="red-text">TENNIS </span>!
+      </h1>
+      <p>
+        Le <span className="blue-text">SAM</span>{" "}
+        <span className="red-text"> Tennis</span> est l'une des 28 sections
+        sportives du <strong>SAM OMNISPORTS</strong>. Il compte de l'ordre de
+        545 licenciés dont 300 ont moins de 18 ans. Ses installations sportives
+        sont sur 2 sites à Mérignac :
+        <ul className="mt-4 ">
+          <li>
+            L'un au 60 Avenue du Truc /{" "}
+            <span className="blue-text">
+              8 courts extérieurs éclairés + 3 cours de Mini-Tennis
+            </span>
+          </li>
+          <li>
+            L'autre au 23 Avenue du Château d'Eau /{" "}
+            <span className="red-text"> 4 courts couverts </span>
+          </li>
+        </ul>
+      </p>
+    </MDBContainer>
+    <MDBContainer>
+      <MDBRow>
+        <MDBCol lg="6">
+          <Inscription />
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
   </Layout>
 )
 
