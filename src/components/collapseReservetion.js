b17bab69aa6ece705484663b303439ceb3b309dc
Sjarj <sebastien.jarjanette@icloud.com>
2019-09-12 19:15:19 +0200

    Start section reservation

--- src/components/collapseReservetion.js
+++ src/components/collapseReservetion.js
@@ -1,52 +1,143 @@
 import React, { Component } from "react"
-import { MDBBtn, MDBCollapse, MDBContainer } from "mdbreact"
+import { Link } from "gatsby"
+import {
+  MDBContainer,
+  MDBTabPane,
+  MDBTabContent,
+  MDBNav,
+  MDBNavItem,
+  MDBIcon,
+} from "mdbreact"
 
 class CollapseReservation extends Component {
   state = {
-    collapseID: "",
+    activeItemClassicTabs3: "1",
   }
 
-  toggleCollapse = collapseID => () => {
-    this.setState(prevState => ({
-      collapseID: prevState.collapseID !== collapseID ? collapseID : "",
-    }))
+  toggleClassicTabs3 = tab => e => {
+    e.preventDefault()
+    if (this.state.activeItemClassicTabs3 !== tab) {
+      this.setState({
+        activeItemClassicTabs3: tab,
+      })
+    }
   }
 
   render() {
     return (
-      <>
-        <h1 className="section-heading text-center mb-5 mt-5 pt-4 font-weight-bold wow fadeIn">
-          Réservation
-        </h1>
-        <MDBContainer className="my-5">
-          <MDBBtn
-            color="primary"
-            href="!#"
-            onClick={this.toggleCollapse("basicCollapse")}
-            style={{ marginBottom: "1rem" }}
+      <MDBContainer>
+        <div className="classic-tabs">
+          <MDBNav classicTabs color="primary" className="mt-5">
+            <MDBNavItem>
+              <Link
+                className={`nav-link ${
+                  this.state.activeItemClassicTabs3 === "1" ? "active" : ""
+                }`}
+                onClick={this.toggleClassicTabs3("1")}
+              >
+                <MDBIcon icon="phone" size="2x" />
+                <br />
+                Par téléphone
+              </Link>
+            </MDBNavItem>
+            <MDBNavItem>
+              <Link
+                className={`nav-link ${
+                  this.state.activeItemClassicTabs3 === "2" ? "active" : ""
+                }`}
+                onClick={this.toggleClassicTabs3("2")}
+              >
+                <MDBIcon icon="at" size="2x" />
+                <br />
+                Par internet
+              </Link>
+            </MDBNavItem>
+            <MDBNavItem>
+              <Link
+                className={`nav-link ${
+                  this.state.activeItemClassicTabs3 === "3" ? "active" : ""
+                }`}
+                onClick={this.toggleClassicTabs3("3")}
+              >
+                <MDBIcon icon="home" size="2x" />
+                <br />
+                Au club house
+              </Link>
+            </MDBNavItem>
+            <MDBNavItem>
+              <Link
+                className={`nav-link ${
+                  this.state.activeItemClassicTabs3 === "4" ? "active" : ""
+                }`}
+                onClick={this.toggleClassicTabs3("4")}
+              >
+                <MDBIcon icon="star" size="2x" />
+                <br />
+                Be Awesome
+              </Link>
+            </MDBNavItem>
+          </MDBNav>
+          <MDBTabContent
+            className="card mb-5"
+            activeItem={this.state.activeItemClassicTabs3}
           >
-            COLLAPSE LINK
-          </MDBBtn>
-          <MDBBtn
-            color="primary"
-            onClick={this.toggleCollapse("basicCollapse")}
-            style={{ marginBottom: "1rem" }}
-          >
-            COLLAPSE BUTTON
-          </MDBBtn>
-          <MDBCollapse id="basicCollapse" isOpen={this.state.collapseID}>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum
-            </p>
-          </MDBCollapse>
-        </MDBContainer>
-      </>
+            <MDBTabPane tabId="1">
+              <p>
+                Par téléphone en appelant le{" "}
+                <strong className="light-blue-text">Club </strong>{" "}
+                <strong className="red-text">House </strong> au :
+                <address>
+                  <a href="tel:+330556974385">05.56.97.43.85</a>{" "}
+                </address>
+                . Les Horaires d'ouverture de notre{" "}
+                <strong className="light-blue-text">Club </strong>{" "}
+                <strong className="red-text">House </strong> sont les suivants:
+              </p>
+              <p>
+                {" "}
+                Le <strong className="light-blue-text">Matin</strong> tous les
+                jours de 9h à 12h30 sauf le Mardi fermé.
+                <br />
+                <strong className="red-text ml-1">L'Après-midi </strong>:
+                <ul>
+                  <li>
+                    De Avril à Septembre. 14h à 20h du Lundi au Samedi, de 14h à
+                    19h le Dimanche.
+                  </li>
+                  <li>
+                    De Octobre à Mars. 14h à 19h du Lundi au Samedi, de 14h à
+                    18h le Dimanche.
+                  </li>
+                </ul>
+              </p>
+            </MDBTabPane>
+            <MDBTabPane tabId="2">
+              <p>
+                Quisquam aperiam, pariatur. Tempora, placeat ratione porro
+                voluptate odit minima. Lorem ipsum dolor sit amet, consectetur
+                adipisicing elit. Nihil odit magnam minima, soluta doloribus
+                reiciendis molestiae placeat unde eos molestias.
+              </p>
+            </MDBTabPane>
+            <MDBTabPane tabId="3">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil
+                odit magnam minima, soluta doloribus reiciendis molestiae
+                placeat unde eos molestias. Quisquam aperiam, pariatur. Tempora,
+                placeat ratione porro voluptate odit minima.
+              </p>
+            </MDBTabPane>
+            <MDBTabPane tabId="4">
+              <p>
+                Quisquam aperiam, pariatur. Tempora, placeat ratione porro
+                voluptate odit minima. Lorem ipsum dolor sit amet, consectetur
+                adipisicing elit. Nihil odit magnam minima, soluta doloribus
+                reiciendis molestiae placeat unde eos molestias.
+              </p>
+            </MDBTabPane>
+          </MDBTabContent>
+        </div>
+      </MDBContainer>
     )
   }
 }
