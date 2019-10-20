6b75504ab5b1a47922b5ea0d7ede701dafb137dc
Sjarj <sebastien.jarjanette@icloud.com>
2019-10-20 16:25:37 +0200

    Competion individuelle done

--- src/components/competionIndividuelles.js
+++ src/components/competionIndividuelles.js
@@ -9,17 +9,17 @@ const CompetitionIndividuelles = () => {
         backgroundImage: `${`url('https://images.unsplash.com/photo-1541744573515-478c959628a0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=975&q=80')`}`,
       }}
     >
-      <div class="flex-center  rgba-pink-strong mask">
-        <div class="container">
+      <div className="flex-center  rgba-pink-strong mask">
+        <div className="container">
           <h3
-            class="text-center pb-4 white-text font-weight-bold wow fadeIn h3-responsive"
+            className="text-center pb-4 white-text font-weight-bold wow fadeIn h3-responsive"
             data-wow-delay="0.2s"
           >
             <strong>Compétion Individuelle</strong>
           </h3>
 
-          <h5
-            className="white-text text-center font-weight-bold pb-4 wow fadeIn h5-responsive"
+          <h6
+            className="white-text text-center font-weight-bold pb-4 wow fadeIn h6-responsive"
             data-wow-delay="0.2s"
           >
             <em>
@@ -27,43 +27,54 @@ const CompetitionIndividuelles = () => {
                 Pour faire votre choix, nous vous donnons les liens suivants :
               </strong>
             </em>
-          </h5>
-          <div class="row text-center">
-            <div class="col-md-4 my-2">
-              <h4 class="white-text mb-1 font-weight-bold h4-responsive">
-                Ligue de Guyenne de Tennis
+          </h6>
+          <div className="row text-center">
+            <div className="col-md-4 my-2">
+              <h4 className="white-text mb-1 font-weight-bold h4-responsive">
+                LIGUE DE NOUVELLE AQUITAINE DE TENNIS
               </h4>
-              <p class="white-text font-weight-bolder">
-                Le but La ligue de Guyenne est d'organiser et de développer la
-                pratique du tennis sur son territoire
-              </p>
-              <MDBBtn rounded outline color="white" className="mt-3">
+              <MDBBtn
+                rounded
+                outline
+                color="white"
+                className="mt-3"
+                href="http://www.ligue.fft.fr/nouvelle-aquitaine/nouvelle-aquitaine_a/cms/index_public.php?us_action=show_note_site&login_off=1&ui_id_site=1"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 Aller sur le site
               </MDBBtn>
             </div>
-
-            <div class="col-md-4 my-2">
-              <h4 class="white-text mb-1 font-weight-bold h4-responsive">
-                Comité Départemental de Gironde
+            <div className="col-md-4 my-2">
+              <h4 className="white-text mb-1 font-weight-bold h4-responsive">
+                COMITÉ DÉPARTEMENTAL DE GIRONDE
               </h4>
-              <p class="white-text font-weight-bolder">
-                Retrouvez toutes les informations sur l'organisation du Tennis
-                en Gironde.
-              </p>
-              <MDBBtn rounded outline color="white" className="mt-4">
+              <MDBBtn
+                rounded
+                outline
+                color="white"
+                className="mt-4"
+                href="http://www.comite.fft.fr/gironde/gironde_a/cms/index_public.php?us_action=show_note_site&login_off=1&ui_id_site=1"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 Aller sur le site
               </MDBBtn>
             </div>
-
-            <div class="col-md-4 my-2">
-              <h4 class="white-text mb-1 font-weight-bold h4-responsive">
-                Fédération Française de Tennis
+            <div className="col-md-4 my-2">
+              <h4 className="white-text mb-1 font-weight-bold h4-responsive">
+                FÉDÉRATION FRANÇAISE DE TENNIS
               </h4>
-              <p class="white-text font-weight-bolder">
-                Retrouvez toute l'actualité du tennis sur le site de la
-                Fédération française de Tennis.
-              </p>
-              <MDBBtn rounded outline color="white" className="mt-3">
+
+              <MDBBtn
+                rounded
+                outline
+                color="white"
+                className="mt-3"
+                href="https://www.fft.fr"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 Aller sur le site
               </MDBBtn>
             </div>
