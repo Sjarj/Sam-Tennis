f78895d506219382079046125e79da2cac9ea0b5
Sjarj <sjarj@imac-de-jarjanette.home>
2020-03-23 19:39:07 +0100

    Fix bug

--- src/components/header.js
+++ src/components/header.js
@@ -6,26 +6,29 @@ const Header = () => {
   return (
     <header>
       <div id="home" className="view ">
-        <div className="parallax" />
-        <div className="mask rgba-black-strong">
-          <div className="container h-100 d-flex justify-content-center align-items-center">
-            <div className="row smooth-scroll">
-              <div className="col-md-12">
-                <div className="white-text text-center">
-                  <MDBAnimation reveal type="fadeIn">
-                    <h2 className="display-3 font-weight-bold ">
-                      <strong>SAM </strong>
-                      <strong>TENNIS</strong>
-                    </h2>
-                  </MDBAnimation>
-                  <MDBAnimation reveal type="fadeIn" delay="0.2s">
-                    <h3>L'une des 28 sections sportives du SAM OMNISPORTS.</h3>
-                  </MDBAnimation>
-                  <MDBAnimation reveal type="fadeIn" delay="0.4s">
-                    <a href="#about" className="btn btn-outline-white">
-                      Rencontre notre équipe
-                    </a>
-                  </MDBAnimation>
+        <div className="parallax">
+          <div className="mask rgba-black-strong">
+            <div className="container h-100 d-flex justify-content-center align-items-center">
+              <div className="row smooth-scroll">
+                <div className="col-md-12">
+                  <div className="white-text text-center">
+                    <MDBAnimation reveal type="fadeIn">
+                      <h2 className="display-3 font-weight-bold ">
+                        <strong>SAM </strong>
+                        <strong>TENNIS</strong>
+                      </h2>
+                    </MDBAnimation>
+                    <MDBAnimation reveal type="fadeIn" delay="0.2s">
+                      <h3>
+                        L'une des 28 sections sportives du SAM OMNISPORTS.
+                      </h3>
+                    </MDBAnimation>
+                    <MDBAnimation reveal type="fadeIn" delay="0.4s">
+                      <a href="#about" className="btn btn-outline-white">
+                        Rencontre notre équipe
+                      </a>
+                    </MDBAnimation>
+                  </div>
                 </div>
               </div>
             </div>
